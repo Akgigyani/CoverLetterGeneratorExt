3819c1f5c089b1944d918c81e4d948910805bee1
Akgigyani <Aamirkgigyani@gmail.com>
2024-05-01 19:58:36 +0100

    changed settings button icon to gear

--- src/components/generator.js
+++ src/components/generator.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { VscGear } from 'react-icons/vsc';
 
 function Generator() {
   return (
@@ -9,7 +10,7 @@ function Generator() {
             </button>
             <h2>LinkedIn Cover Letter Generator</h2>
             <button className='border mr-[1px] p-2 border-solid border-gray-600 rounded-[100%] hover:border-2 hover:mr-0 transition duration-300 ease-in-out'>
-                Settings
+                <VscGear />
             </button>
         </div>
         {/*  */}
